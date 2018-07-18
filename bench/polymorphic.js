0c95ac6092207fa4b2a88d54d1561bbbe84715a8
Sergei Dryganets <sergeyd@microsoft.com>
2018-07-18 16:38:25 -0700

    experiment with polymorphic test

--- bench/polymorphic.js
+++ bench/polymorphic.js
@@ -5,7 +5,7 @@ var suite = new benchmark.Suite()
 var runs = 0
 
 suite.add('polymorphic', function polymorphic() {
-  var objects = [{a:1}, {b:1, a:2}, {c:1, b:2, a:3}, {d:1, c:2, b:3, a:4}];
+  var objects = [{b:1 ,a:1, c:7}, {c: 7, b:1, a:2}, {c:1, a:3, b:2}, {a:4, b:3, c:4}];
   var sum = 0;
   for (var i = 0; i < 10000; i++) {
     var o = objects[i & 3];
@@ -13,6 +13,17 @@ suite.add('polymorphic', function polymorphic() {
   }
   return sum;
 })
+
+suite.add('sorted', function sorted() {
+  var objects = [{a:1 ,b:1, c:7}, {a: 2, b:1, c:7}, {a:3, b:2, c:1}, {a:4, b:3, c:4}];
+  var sum = 0;
+  for (var i = 0; i < 10000; i++) {
+    var o = objects[i & 3];
+    sum += o.a;
+  }
+  return sum;
+})
+
 
 suite.add('monomorphic', function monomorphic() {
   var objects = [{a:1}, {a:2}, {a:3}, {a:4}];
